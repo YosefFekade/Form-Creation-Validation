8cfe5aabd28aa9aad3de073e0123af5c78342adb
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-07-18 11:55:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ const feedbackDiv= document.getElementById('form-feedback');
       const emailInput = document.getElementById('email');
       const passwordInput = document.getElementById	('password');
     
-      const usernametrimed = usernameInput.value.trim();
-      const emailtimrd = emailInput.value.trim();
-      const passwordtrimed = passwordInput.value.trim();
+      const username = usernameInput.value.trim();
+      const email = emailInput.value.trim();
+      const password = passwordInput.value.trim();
 
       var isValid = true;
       let messages =[];
 
-      if (usernametrimed.value.length < 3) {
+      if (username.length < 3) {
             isValid=false;
             messages.push  ("The user name must be more than 3 characters");
             return;
@@ -24,21 +24,21 @@ const feedbackDiv= document.getElementById('form-feedback');
             isValid = true;
           }
 
-      if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailtimrd.value)) {
+      if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email .value)) {
              isValid = false;
-             messages.push ('Insert an approprite email adress');
+             messages.push ("Insert an approprite email adress");
             return;
           } else {
             isValid = true;
           }
-     if (passwordtrimed.value.length < 8) {
+     if (password.length < 8) {
              isValid = false;
-             messages.push ('Password must be at least 8 charachters long');
+             messages.push ("Password must be at least 8 charachters long");
             return;
               } else {
                 isValid = true;
               }
-    function handleRegistration(isValid, messages) {
+              function handleRegistration(isValid, messages) {
                 const feedbackDiv =  document.getElementById('form-feedback');
               
                 feedbackDiv.style.display = "block";
